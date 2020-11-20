08d218a74cd63cd82cc3004b6916972cc4155368
Melissa Da Costa <melicheg@gmail.com>
2020-11-20 12:10:31 +0100

    Projects

--- js/index.js
+++ js/index.js
@@ -1,42 +1,48 @@
-//Control SideBar
-$(document).ready(function() {
-    var fixHeight = function() {
-      $('.navbar-nav').css(
-        'max-height',
-        document.documentElement.clientHeight - 150
-      );
-    };
-    fixHeight();
-    $(window).resize(function() {
-      fixHeight();
-    });
-    $('.navbar #hamburguer').on('click', function() {
-      fixHeight();
-    });
-    $('#hamburguer').on('click', function() {
-      $('.mobileMenu').toggleClass('open');
-    });
-  });
+$(function() {
 
+	// Get the form.
+	var form = $('#contact-form');
 
-  $(document).ready(function () {
-    $("#sidebar").mCustomScrollbar({
-        theme: "minimal"
-    });
+	// Get the messages div.
+	var formMessages = $('.form-message');
 
-    $('#dismiss, .overlay').on('click', function () {
-        // hide sidebar
-        $('#sidebar').removeClass('active');
-        // hide overlay
-        $('.overlay').removeClass('active');
-    });
+	// Set up an event listener for the contact form.
+	$(form).submit(function(e) {
+		// Stop the browser from submitting the form.
+		e.preventDefault();
 
-    $('#sidebarCollapse').on('click', function () {
-        // open sidebar
-        $('#sidebar').addClass('active');
-        // fade in the overlay
-        $('.overlay').addClass('active');
-        $('.collapse.in').toggleClass('in');
-        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-    });
+		// Serialize the form data.
+		var formData = $(form).serialize();
+
+		// Submit the form using AJAX.
+		$.ajax({
+			type: 'POST',
+			url: $(form).attr('action'),
+			data: formData
+		})
+		.done(function(response) {
+			// Make sure that the formMessages div has the 'success' class.
+			$(formMessages).removeClass('error');
+			$(formMessages).addClass('success');
+
+			// Set the message text.
+			$(formMessages).text(response);
+
+			// Clear the form.
+			$('#contact-form input,#contact-form textarea').val('');
+		})
+		.fail(function(data) {
+			// Make sure that the formMessages div has the 'error' class.
+			$(formMessages).removeClass('success');
+			$(formMessages).addClass('error');
+
+			// Set the message text.
+			if (data.responseText !== '') {
+				$(formMessages).text(data.responseText);
+			} else {
+				$(formMessages).text('Oops! Une erreur s\'est produite et votre message n\'a pas pu être envoyé.');
+			}
+		});
+	});
+
 });
